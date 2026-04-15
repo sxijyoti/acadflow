cf413b50f0b2edf3c8575be7c86753bfe5a666ba
Samridhi-2309 <sam23092004@gmail.com>
2026-04-15 10:12:50 +0530

    refactor: add DTO layer

---
+++ src/main/java/com/acadflow/module1/dto/EnrollmentRequestDTO.java
@@ -0,0 +1,6 @@
+package com.acadflow.module1.dto;
+
+public class EnrollmentRequestDTO {
+    public Long userId;
+    public Long subjectId;
+}
---
+++ src/main/java/com/acadflow/module1/dto/SubjectCreateDTO.java
@@ -0,0 +1,8 @@
+package com.acadflow.module1.dto;
+
+public class SubjectCreateDTO {
+    public String name;
+    public String code;
+    public Integer credits;
+    public Long instructorId;
+}
