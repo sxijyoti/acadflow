1eb3a1a396cb1cf13d523e33160ad0da482c16cc
Rohini-Lakkimsetty <rohinilakkimsetty2005@gmail.com>
2026-04-15 21:12:08 +0530

    Implement resource upload and retrieval by subject

--- src/main/java/com/acadflow/module2/repository/ResourceRepository.java
+++ src/main/java/com/acadflow/module2/repository/ResourceRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//fixed
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
